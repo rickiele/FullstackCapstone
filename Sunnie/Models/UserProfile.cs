ba5316b35f9b99c3a946f0d21fdffa19cf85a0b8
Rickie Le <rickienthefishies@gmail.com>
2021-05-04 18:37:43 -0500

    added dbutils.cs

---
+++ Sunnie/Models/UserProfile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Sunnie.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [StringLength(28, MinimumLength = 28)]
+        public string FirebaseId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        public string CreateDateTime { get; set; }
+
+        public int Age { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Url)]
+        [MaxLength(255)]
+        public string ImageLocation { get; set; }
+
+        [Required]
+        public int SkinTypeId {get; set;}
+        public SkinType SkinType { get; set; }
+    }
+}
