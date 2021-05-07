51a4bf2fecf868e5b913739d074aea4cf4a844c0
Rickie Le <rickienthefishies@gmail.com>
2021-05-06 19:34:30 -0500

    added product and productType models

---
+++ Sunnie/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sunnie.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+        public string Name { get; set; }
+        public string ImageLocation { get; set; }
+        public int TypeId { get; set; }
+        public ProductType Type { get; set; }
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public int Spf { get; set; }
+        public string Comment { get; set; }
+    }
+}
