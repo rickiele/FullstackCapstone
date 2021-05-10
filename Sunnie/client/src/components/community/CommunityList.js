cd7d16d08f9dfa69b69e5f23ceec041817c1ee97
Rickie Le <rickienthefishies@gmail.com>
2021-05-10 01:38:45 -0500

    community doesn't show logged in user anymore

--- Sunnie/client/src/components/community/CommunityList.js
+++ Sunnie/client/src/components/community/CommunityList.js
@@ -6,24 +6,35 @@ import { Container } from "react-bootstrap"
 export const CommunityList = () => {
 
     const { userProfiles, getAllUserProfiles } = useContext(UserProfileContext);
+    const currentUser = JSON.parse(sessionStorage.getItem("userProfile"));
+
 
     useEffect(() => {
         getAllUserProfiles();
     }, []);
 
+    // {
+    //     userProfiles.filter(userProfile => currentUser.id =)
+    //         return <ProductCard key={product.id} product={product} />
+    //     })
+    // }
+
+    const filterCommunity = userProfiles.filter(userProfile => userProfile.id != currentUser.id)
+
     //Need to add a filter by skin type
     // Need to not show the current user in the array
 
-    console.log(userProfiles, "hello??")
+    console.log(filterCommunity, "oink??")
     return (
         <Container className="container">
             <h1>Community</h1>
             <p>Check out how other users protect themselves from the sun.</p>
             {
-                userProfiles.map(userProfile => {
-                    return <UserCard key={userProfile.id} userProfile={userProfile} />
-                })
-            }
+                userProfiles.filter(userProfile => userProfile.id != currentUser.id).map(userProfile => (
+                    <UserCard key={userProfile.id} userProfile={userProfile} />
+                ))}
+
         </Container>
     );
 }
+
