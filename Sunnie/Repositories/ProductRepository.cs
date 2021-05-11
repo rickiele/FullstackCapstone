5fe6462cafaadcf185b0b07df6e1d372df756b33
Rickie Le <rickienthefishies@gmail.com>
2021-05-11 14:04:58 -0500

    product deletes

--- Sunnie/Repositories/ProductRepository.cs
+++ Sunnie/Repositories/ProductRepository.cs
@@ -95,7 +95,8 @@ namespace Sunnie.Repositories
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"DELETE FROM Product WHERE Id = @id;
+                    cmd.CommandText = @"DELETE FROM Product WHERE Id = @id
+                                        DELETE FROM Favorite WHERE ProductId = @id
                                         ";
 
                     DbUtils.AddParameter(cmd, "@id", productId);
